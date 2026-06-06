50c22afbabf6bd7af3609a18f752feaccbcd5b21
Scott Hughes <scottcreatescode@gmail.com>
2026-06-05 22:30:26 -0400

    Add files via upload

---
+++ Config/nvim/init.lua
@@ -0,0 +1,4 @@
+vim.api.nvim_set_hl(0, "Normal", { bg = "NONE", ctermbg = "NONE" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE", ctermbg = "NONE" })
+vim.api.nvim_set_hl(0, "FloatBorder", {bg = "NONE", ctermbg = "NONE" })
+vim.api.nvim_set_hl(0, "Pmenu", {bg = "NONE", ctermbg = "NONE" })
